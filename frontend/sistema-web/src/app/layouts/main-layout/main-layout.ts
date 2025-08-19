343088d156c3bee38f706b7e61d5a8c841b5c25e
Luiz Gustavo <luizguaycurus@gmail.com>
2025-08-19 14:12:19 -0300

    style(dashboard, main-layout): new buttons

--- frontend/sistema-web/src/app/layouts/main-layout/main-layout.ts
+++ frontend/sistema-web/src/app/layouts/main-layout/main-layout.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-main-layout',
@@ -13,7 +14,14 @@ import { SidebarComponent } from '../../components/sidebar/sidebar';
 export class MainLayoutComponent {
   isSidebarCollapsed = false;
 
+  constructor(private authService: AuthService, private router: Router){}
+
   toggleSidebar() : void{
     this.isSidebarCollapsed = !this.isSidebarCollapsed;
+  }
+
+  onLogout(): void{
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
--- frontend/sistema-web/src/app/pages/dashboard/dashboard.ts
+++ frontend/sistema-web/src/app/pages/dashboard/dashboard.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlunoService } from '../../services/aluno';
 import { Aluno } from '../../interfaces/aluno.interface';
+import { AlunoFormComponent } from '../../components/aluno-form/aluno-form';
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AlunoFormComponent],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
 export class DashboardComponent implements OnInit {
   alunos: Aluno[] = [] // array pra armazenar os alunos
   isLoading = true; // variavel pra controlar o estado do carregamento
+  isModalVisible = false;
 
   constructor(private alunoService: AlunoService){}
 
@@ -34,5 +36,17 @@ export class DashboardComponent implements OnInit {
         this.isLoading = false;
       }
     });
+  }
+
+  abrirModal():void{
+    this.isModalVisible=true;
+  }
+
+  fecharModal():void{
+    this.isModalVisible=false;
+  }
+
+  onAlunoCriado(novoAluno: Aluno){
+    this.alunos.push(novoAluno);
   }
 }
